8aebd26507defa70dc9058607ff6e88a2bd5dbc6
arekflis <arkadiuszflisikowski10@gmail.com>
2025-03-06 00:30:59 +0100

    Add Dockerfile for all microservices and create docker-compose.yaml

---
+++ spring-boot/clubMicroservice/build.sh
@@ -0,0 +1,6 @@
+title="$(grep -n "org.opencontainers.image.title" Dockerfile | cut -f2 -d "=" | xargs)"
+version="$(grep -n "org.opencontainers.image.version" Dockerfile | cut -f2 -d "=" | xargs)"
+
+docker build \
+    --label "org.opencontainers.image.created=$(date +%Y-%m-%dT%H:%M:%S%:z)" \
+    -t "${title}":"${version}" .
